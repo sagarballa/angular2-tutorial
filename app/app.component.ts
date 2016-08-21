6e4b5ad137fc26d3080650a60b07019408c619df
Sagar Balla <sagar.balla@gmail.com>
2016-08-21 21:21:43 +0530

    Tutorial Service

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -76,13 +77,25 @@ const HEROES: Hero[] = [
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+    providers: [HeroService]
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+    constructor(private heroService: HeroService) { }
+
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
