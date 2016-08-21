fbe3b1c22e54ebd891b64fe1ed6b44d601e30ebf
Sagar Balla <sagar.balla@gmail.com>
2016-08-21 23:20:07 +0530

    Tutorial Routes step 3

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: 'app/dashboard.component.html'
+  templateUrl: 'app/dashboard.component.html',
+    styleUrls: ['app/dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit{ 
     heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {
+    }
 
     getHeroes(): void {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
@@ -21,5 +26,8 @@ export class DashboardComponent implements OnInit{
         this.getHeroes();
     }
 
-    gotoDetail(hero: Hero): void { /* not implemented yet */}
+   gotoDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+   }
 }
